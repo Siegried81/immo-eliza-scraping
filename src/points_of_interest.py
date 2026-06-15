ba7b84b0ebf2d8003ee24eb15097269f76cc1e51
max <max.huberland@hotmail.fr>
2026-06-15 15:41:43 +0200

    Adding comments

--- src/points_of_interest.py
+++ src/points_of_interest.py
@@ -1,12 +1,21 @@
 
 class Interests_parser():
+    """
+    Module parsing a html page in the form of a BeautifullSoup to return a 
+    dictionary of three relevant points of interest near a property.
+    """
     def __init__(self):
         pass
     
     def parsing(self, soup):
+        """
+        Method making the dictionary, all values iniated as None then attempt to fill them using other methods
+        :param: a BeautifullSoup
+        returns a dictionary
+        """
         points_of_interest = {"Preschool_distance": None, "Train_station_distance": None, "Supermarket_distance": None}
         for i in range(1, 4):
-
+            # Taking the first three tabs of the 'Point of Interest' section
             tab = soup.find("div", attrs={"id": f"tabs-{i}"})
             if tab:
                 if i == 1:
@@ -19,6 +28,11 @@ class Interests_parser():
         return points_of_interest
 
     def parsing_preschool(self, tab):
+        """
+        Method finding the distance to the nearest preschool
+        :param: a part of a BeautifullSoup
+        returns an integer
+        """
         #list of sub-titles
         tab_titles = [elem.text.strip() for elem in tab.find_all("h3")]
         if not "Preschools" in tab_titles:
@@ -29,6 +43,11 @@ class Interests_parser():
         return self.convert_to_meters(distance)
     
     def parsing_train_station(self, tab):
+        """
+        Method finding the distance to the train station
+        :param: a part of a BeautifullSoup
+        returns an integer
+        """
         #list of sub-titles
         tab_titles = [elem.text.strip() for elem in tab.find_all("h3")]
         if "Train stations" not in tab_titles:
@@ -40,8 +59,18 @@ class Interests_parser():
         return self.convert_to_meters(distance)
 
     def parsing_supermarket(self, tab):
+        """
+        Method finding the distance to the nearest supermarket
+        :param: a part of a BeautifullSoup
+        returns an integer
+        """
         distance = tab.find("span", attrs={"title": "Walking"}).text.strip().split()[:2]
         return self.convert_to_meters(distance)
     
     def convert_to_meters(self, distance):
+        """
+        Method to convert
+        :param: list [numerical value as string, unit(m|km) as string]
+        returns an integer
+        """
         return int(distance[0]) if distance[1] == 'm' else int(float(distance[0]) * 1000)
