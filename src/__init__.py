c1b66bc5329af0640dedf8c8d50c3fa5d7ee4d3c
Siegried Camus <csiegried@yahoo.fr>
2026-06-17 12:28:34 +0200

    Import run_url_threads and fetch_data

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,4 @@
 from .html_scraper import parse_property, to_json_file
 from .points_of_interest import Interests_parser
 from .url_fetcher import run
+from .Thread_URL_prov_region import run_url_threads, fetch_data
